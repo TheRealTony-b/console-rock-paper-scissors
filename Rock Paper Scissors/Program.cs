f36fa92d6c2550f1faa5c6a6fb0ddf98cb6bc321
Tony <79669121+TheRealTony-b@users.noreply.github.com>
2021-02-26 15:22:19 -0500

    Some variables are named in french, sorry about that

---
+++ Rock Paper Scissors/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Rock_Paper_Scissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ScoreJoueur = 0 ;
+            int ScoreOrdi = 0 ;
+           
+            if (ScoreJoueur == 3)
+            {
+                Console.WriteLine("You won the game!" + ScoreJoueur + "-" + ScoreOrdi);
+            }
+            if (ScoreOrdi == 3)
+            {
+                Console.WriteLine("You lost the game!" + ScoreJoueur + "-" + ScoreOrdi);
+            }
+            while (ScoreJoueur < 3 & ScoreOrdi < 3)
+            {        
+            Console.WriteLine("Rock(1) Paper(2) Scissors(3)");
+            int joueur = Convert.ToInt32 (Console.ReadLine());
+            
+            Random rnd = new Random();
+            int ordi = rnd.Next(1, 3);
+            if (joueur == ordi)
+            {
+                Console.WriteLine("Draw!");
+            }                
+            switch(ordi)
+            {
+                case 1:
+                    Console.WriteLine("The computer chose Rock");
+                    if (joueur == 2)
+                    {
+                            ScoreJoueur++;
+                            Console.WriteLine("You won the game!" + ScoreJoueur + "-" + ScoreOrdi);
+                        
+                    }
+                    else if (joueur == 3)
+                    {
+                            ScoreOrdi++;
+                            Console.WriteLine("You lost the game :(" + ScoreJoueur + "-" + ScoreOrdi);
+                        
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("The computer chose paper ");
+                    if (joueur == 3)                 
+                    {
+                            ScoreJoueur++;
+                            Console.WriteLine("You won the game" + ScoreJoueur + "-" + ScoreOrdi);
+                        
+                    }
+                    else if (joueur == 1)
+                    {
+                            ScoreOrdi++;
+                            Console.WriteLine("You lost the game :(" + ScoreJoueur + "-" + ScoreOrdi);
+                        
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("The computer chose scissors");
+                    if (joueur == 1)
+                    {
+                            ScoreJoueur++;
+                            Console.WriteLine("You won the game!" + ScoreJoueur + "-" + ScoreOrdi);
+                       
+                    }
+                    else if (joueur == 1)
+                    {
+                            ScoreOrdi++;
+                            Console.WriteLine("You lost the game :(" + ScoreJoueur + "-" + ScoreOrdi);
+                        
+                    }
+                    break;
+                }
+
+            }
+
+            
+        }
+    }
+}
